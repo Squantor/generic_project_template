1ab779fc6c796729a7c2fc38a31f069c5a5f84c9
Squantor <squantmuts@gmail.com>
2021-01-25 21:41:25 +0100

    added target from libMcuLL that was new

---
+++ software/targets/LPC824M201DH20.mk
@@ -0,0 +1,12 @@
+# SPDX-License-Identifier: MIT
+# Copyright (c) 2021 Bart Bilos
+# For conditions of distribution and use, see LICENSE file
+
+# settings for the LPC824M201DH20 Microcontroller
+#
+# Version: 20210112
+
+# import compiler settings for the general controller type
+include targets/LPC824.mk
+
+

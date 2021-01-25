84e58696ee8a075d6efeb27e5faffc4ff996cfa4
Squantor <squantmuts@gmail.com>
2021-01-25 22:02:46 +0100

    updated rules files and differences

--- software/LPC1125/LPC1125_rules.mk
+++ software/LPC1125/LPC1125_rules.mk
@@ -1,39 +1,10 @@
-# MIT License
+# SPDX-License-Identifier: MIT
+# Copyright (c) 2021 Bart Bilos
+# For conditions of distribution and use, see LICENSE file
 
-# Copyright (c) 2020 Bart Bilos
-
-# Permission is hereby granted, free of charge, to any person obtaining a copy
-# of this software and associated documentation files (the "Software"), to deal
-# in the Software without restriction, including without limitation the rights
-# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-# copies of the Software, and to permit persons to whom the Software is
-# furnished to do so, subject to the following conditions:
-
-# The above copyright notice and this permission notice shall be included in all
-# copies or substantial portions of the Software.
-
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-# SOFTWARE.
-
-# Project specific rules, a few predefined but feel free to add your own
+# Project specific rules with a few predefined, extend as needed
 #
-# Version: 20210108
-
-# executed 
-pre-build:
-	$(U) "executing pre build steps"
-
-.PHONY: pre-build
-
-post-build: main-build
-	$(U) "executing post build steps"
-
-.PHONY: post-build
+# Version: 20210113
 
 #project hardware specific commands
 gdbbmp: all
@@ -48,4 +19,13 @@ tpwrenable:
 	$(TOOLCHAIN_PREFIX)$(GDB) -x ./gdb_scripts/bmp_tpwr_enable.txt
 .PHONY: tpwrenable
 
+# always executed by build engine
+pre-build:
+	@echo executing pre build steps
 
+.PHONY: pre-build
+
+post-build: main-build
+	@echo executing post build steps
+
+.PHONY: post-build
--- software/LPC824/LPC824.mk
+++ software/LPC824/LPC824.mk
@@ -1,36 +1,19 @@
-# MIT License
-
-# Copyright (c) 2020 Bart Bilos
-
-# Permission is hereby granted, free of charge, to any person obtaining a copy
-# of this software and associated documentation files (the "Software"), to deal
-# in the Software without restriction, including without limitation the rights
-# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-# copies of the Software, and to permit persons to whom the Software is
-# furnished to do so, subject to the following conditions:
-
-# The above copyright notice and this permission notice shall be included in all
-# copies or substantial portions of the Software.
-
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-# SOFTWARE.
+# SPDX-License-Identifier: MIT
+# Copyright (c) 2021 Bart Bilos
+# For conditions of distribution and use, see LICENSE file
 
 # project settings
 #
 # Version: 20201220
 
 #additional library includes
+include libMcuLL/libMcuLL.mk
 include squantorLibC/squantorLibC.mk
 include squantorLibEmbeddedC/squantorLibEmbeddedC.mk
 include libEmbedded/libEmbedded.mk
 
 # project settings
-MCU = LPC824M201JHI33
+MCU = LPC824M201DH20
 TARGET = MCU
 BOARD = dummy_board
 
@@ -38,6 +21,6 @@ BOARD = dummy_board
 FILES += $(PROJECT)/src/main.cpp \
 $(PROJECT)/src/$(BOARD).cpp
 
-INCLUDES += -IlibMcuLL/inc -I$(PROJECT)/inc
+INCLUDES += -I$(PROJECT)/inc
 
 

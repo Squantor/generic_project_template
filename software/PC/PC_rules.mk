cc43259bb667a04ccda84945ad38c8a01dd49fb4
Squantor <squantmuts@gmail.com>
2021-02-07 21:55:40 +0100

    added postbuild step of generating assembly listing of the final program. The listing has no instruction disassembly nor addresses, making it easy to compare them between them

--- software/PC/PC_rules.mk
+++ software/PC/PC_rules.mk
@@ -2,25 +2,30 @@
 # Copyright (c) 2021 Bart Bilos
 # For conditions of distribution and use, see LICENSE file
 
-# Project specific rules, a few predefined but feel free to add your own
+# Project specific rules with a few predefined, extend as needed
 #
-# Version: 20210115
+# Version: 20210207
 
-# project specific rules
-execute: all
-	$(EXECUTABLE)
-
-.PHONY: execute
-
-# executed 
+# always executed by build engine
 pre-build:
-	$(U) "executing pre build steps"
+	@echo executing pre build steps
 
 .PHONY: pre-build
 
-post-build: main-build
-	$(U) "executing post build steps"
+post-build: main-build generate-diassembly
+	@echo executing post build steps
 
 .PHONY: post-build
 
+DATESTRING := $(shell date +%Y%m%d%H%M%S)
+
+# generate disassembly of currently built executable
+generate-diassembly: $(EXECUTABLE) $(PROJECT)/disassemblies/build-tag
+	$(C)$(TOOLCHAIN_PREFIX)$(OBJDUMP) -h --no-show-raw-insn --no-addresses -S "$(EXECUTABLE)" > "$(PROJECT)/disassemblies/$(PROJECT)_$(DATESTRING).lss"
+
+.PHONY: generate-diassembly
+
+$(PROJECT)/disassemblies/build-tag: 
+	$(C)$(MKDIR) -p $(PROJECT)/disassemblies
+	$(C)$(TOUCH) $@
 
--- software/PC_bare/PC_bare_rules.mk
+++ software/PC_bare/PC_bare_rules.mk
@@ -1,38 +1,31 @@
-# MIT License
+# SPDX-License-Identifier: MIT
+# Copyright (c) 2021 Bart Bilos
+# For conditions of distribution and use, see LICENSE file
 
-# Copyright (c) 2020 Bart Bilos
-
-# Permission is hereby granted, free of charge, to any person obtaining a copy
-# of this software and associated documentation files (the "Software"), to deal
-# in the Software without restriction, including without limitation the rights
-# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-# copies of the Software, and to permit persons to whom the Software is
-# furnished to do so, subject to the following conditions:
-
-# The above copyright notice and this permission notice shall be included in all
-# copies or substantial portions of the Software.
-
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-# SOFTWARE.
-
-# Project specific rules, a few predefined but feel free to add your own
+# Project specific rules with a few predefined, extend as needed
 #
-# Version: 20200722
+# Version: 20210207
 
-# executed 
+# always executed by build engine
 pre-build:
-	$(U) "executing pre build steps"
+	@echo executing pre build steps
 
 .PHONY: pre-build
 
-post-build: main-build
-	$(U) "executing post build steps"
+post-build: main-build generate-diassembly
+	@echo executing post build steps
 
 .PHONY: post-build
 
+DATESTRING := $(shell date +%Y%m%d%H%M%S)
+
+# generate disassembly of currently built executable
+generate-diassembly: $(EXECUTABLE) $(PROJECT)/disassemblies/build-tag
+	$(C)$(TOOLCHAIN_PREFIX)$(OBJDUMP) -h --no-show-raw-insn --no-addresses -S "$(EXECUTABLE)" > "$(PROJECT)/disassemblies/$(PROJECT)_$(DATESTRING).lss"
+
+.PHONY: generate-diassembly
+
+$(PROJECT)/disassemblies/build-tag: 
+	$(C)$(MKDIR) -p $(PROJECT)/disassemblies
+	$(C)$(TOUCH) $@
 

6abea50bea81db391d6087c8d655ab6d0a4d6bd0
Squantor <squantmuts@gmail.com>
2022-06-06 18:03:57 +0200

    LPC824 now builds

--- software/LPC824/src/dummy_board.cpp
+++ software/LPC824/src/dummy_board.cpp
@@ -6,18 +6,4 @@ For conditions of distribution and use, see LICENSE file
 */
 #include <board.hpp>
 
-void boardInit(void) {
-  // setup system clocks
-  ClockSetPLLBypass(false, false);
-  // SysctlPowerUp(SYSCTL_SLPWAKE_SYSOSC_PD);
-  ClockSetSystemPLLSource(SYSCTL_PLLCLKSRC_IRC);
-  // ClockSetSystemPLLSource(SYSCTL_PLLCLKSRC_SYSOSC);
-  FmcSetFlashAccess(FLASHTIM_30MHZ_CPU);
-  SysctlPowerDown(SYSCTL_SLPWAKE_SYSPLL_PD);
-  ClockSetupSystemPLL(4, 1);
-  SysctlPowerUp(SYSCTL_SLPWAKE_SYSPLL_PD);
-  while (!ClockIsSystemPLLLocked())
-    ;
-  ClockSetSysClockDiv(2);
-  ClockSetMainClockSource(SYSCTL_MAINCLKSRC_PLLOUT);
-}
+void boardInit(void) {}

a514d839048464a75a863e7d4e922d9b6234eca9
Squantor <squantmuts@gmail.com>
2022-06-08 21:36:04 +0200

    LPC1125 builds, debug support needs to be added to black magic probe

--- software/LPC1125BD48_303/inc/board.hpp
+++ software/LPC1125BD48_303/inc/board.hpp
@@ -7,13 +7,15 @@ For conditions of distribution and use, see LICENSE file
 #ifndef BOARD_HPP
 #define BOARD_HPP
 
-#define CLOCK_MAIN_SOURCE   SYSCTL_MAINCLKSRC_PLLOUT
+#define CLOCK_MAIN_SOURCE SYSCTL_MAINCLKSRC_PLLOUT
 
-#define CLOCK_XTAL          (12000000u)
-#define CLOCK_EXT_IN        (0u)
-#define CLOCK_CPU           (30000000u)
-#define CLOCK_AHB           (30000000u)
-#define CLOCK_MAIN          (60000000u)
+#define CLOCK_XTAL (12000000u)
+#define CLOCK_EXT_IN (0u)
+#define CLOCK_CPU (30000000u)
+#define CLOCK_AHB (30000000u)
+#define CLOCK_MAIN (60000000u)
+
+//#include <mcu_ll.h>
 
 void boardInit(void);
 

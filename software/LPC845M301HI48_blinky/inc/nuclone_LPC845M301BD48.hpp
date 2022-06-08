d1d3746c03b63efb4c8a56a843cd23cef58519f5
Squantor <squantmuts@gmail.com>
2022-06-08 21:07:05 +0200

    LPC845 blinky works

--- software/LPC845M301HI48_blinky/inc/nuclone_LPC845M301BD48.hpp
+++ software/LPC845M301HI48_blinky/inc/nuclone_LPC845M301BD48.hpp
@@ -7,24 +7,24 @@
 #ifndef NUCLONE_LPC845M301BD48_HPP
 #define NUCLONE_LPC845M301BD48_HPP
 
-#define CLOCK_MAIN_SOURCE   SYSCTL_MAINCLKSRC_PLLOUT
+#define CLOCK_MAIN_SOURCE SYSCTL_MAINCLKSRC_PLLOUT
 
-#define CLOCK_XTAL          (12000000u)
-#define CLOCK_EXT_IN        (0u)
-#define CLOCK_CPU           (30000000u)
-#define CLOCK_AHB           (30000000u)
-#define CLOCK_MAIN          (60000000u)
+#define CLOCK_XTAL (12000000u)
+#define CLOCK_EXT_IN (0u)
+#define CLOCK_CPU (30000000u)
+#define CLOCK_AHB (30000000u)
+#define CLOCK_MAIN (60000000u)
 
 #include <mcu_ll.h>
 
-#define TICKS_PER_S         2
+#define TICKS_PER_S 6
 
-#define IOCON_XTAL_IN       IOCON_PIO0_8
-#define IOCON_XTAL_OUT      IOCON_PIO0_9
+#define IOCON_XTAL_IN IOCON_PIO0_8
+#define IOCON_XTAL_OUT IOCON_PIO0_9
 
-#define IOCON_LED           IOCON_PIO0_13
-#define PORT_LED            GPIO_PORT0
-#define PIN_LED             (13u)
+#define IOCON_LED IOCON_PIO0_13
+#define PORT_LED GPIO_PORT0
+#define PIN_LED (13u)
 
 void boardInit(void);
 

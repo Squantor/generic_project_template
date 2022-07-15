6d33cd1dd2ebdf166dedeee8ceb3ea2c958c1cf8
Squantor <squantmuts@gmail.com>
2022-07-15 10:18:21 +0200

    silencing warnings

--- software/PC/src/main.cpp
+++ software/PC/src/main.cpp
@@ -8,10 +8,10 @@ For conditions of distribution and use, see LICENSE file
 #include <stdio.h>
 #include <iostream>
 
-int main(int argc, char *argv[])
-//int main()
-{
-    testfunc();
-    printf("Hello World!\n");
-    return 0;
+int main(int argc, char *argv[]) {
+  (void)argc;
+  (void)argv;
+  testfunc();
+  printf("Hello World!\n");
+  return 0;
 }
